49c7e4693f5b51253329ebfb060759fa421d1da7
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2022-12-28 18:25:01 -0600

    Program updated

--- temperature-converter.py
+++ temperature-converter.py
@@ -4,3 +4,9 @@ K = 273 + C
 F = 1,8*C + 32
 '''
 
+centigrades = int(input("Give me the °C to convert: "))
+K = 273 + centigrades
+F = 1.8 * centigrades + 32
+
+print("f{centigrades}°C are equivalent to: " )
+print(f"{K}K and {F}°F")
